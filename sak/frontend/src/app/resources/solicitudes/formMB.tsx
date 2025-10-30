0045f010ed21bf39e56d9a5e44655cb7962f031c
gustavo2866 <gustavo.palmieri@hotmail.com>
2025-10-29 23:58:05 -0300

    gcp017-formmb combo

--- sak/frontend/src/app/resources/solicitudes/formMB.tsx
+++ sak/frontend/src/app/resources/solicitudes/formMB.tsx
@@ -472,7 +472,7 @@ export const SolicitudFormMB = () => {
                     <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                   </Button>
                 </PopoverTrigger>
-                <PopoverContent className="w-full p-0" align="start">
+                <PopoverContent className="w-full p-0" align="start" side="bottom" sideOffset={4}>
                   <Command>
                     <CommandInput placeholder="Buscar artículo..." />
                     <CommandList>
