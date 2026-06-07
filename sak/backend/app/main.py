889c2a71b82ec1195b83b9543f9005b7a9596b90
gustavo2866 <gustavo.palmieri@hotmail.com>
2026-06-07 17:30:55 -0300

    dev-027 (fix)

--- sak/backend/app/main.py
+++ sak/backend/app/main.py
@@ -7,7 +7,7 @@ import os
 import logging
 import traceback
 
-# Configurar logging xxx22
+# Configurar logging xxx223344
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
